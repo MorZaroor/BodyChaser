798b4a976572eb9741a0c34d96cefe78d418e3c3
284768 <game22403004@gmail.com>
2024-11-22 12:13:23 +0900

    プレイヤー_11/22
    スピードパラメーター

--- BodyChaser/Assets/HIroki/Script/Player.cs
+++ BodyChaser/Assets/HIroki/Script/Player.cs
@@ -6,26 +6,27 @@ using UnityEngine.InputSystem;
 public class Player : MonoBehaviour
 {
     [SerializeField, Header("c‚ÌˆÚ“®‘¬“x")]
-    private float _tate_speed;
+    private float _Tate_speed;
     [SerializeField, Header("‰¡‚ÌˆÚ“®‘¬“x")]
-    private float _yoko_speed;
+    private float _Yoko_speed;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        // 120fps‚ğ–Ú•W‚Éİ’è
+        Application.targetFrameRate = 120;
     }
 
     // Update is called once per frame
     void Update()
     {
-        float moveX = Input.GetAxis("Horizontal") * Time.deltaTime * _yoko_speed;
-        float moveY = Input.GetAxis("Vertical") * Time.deltaTime * _tate_speed;
+        float moveX = Input.GetAxis("Horizontal") * Time.deltaTime * _Yoko_speed;
+        float moveY = Input.GetAxis("Vertical") * Time.deltaTime * _Tate_speed;
 
         transform.position = new Vector2(
             transform.position.x,
             Mathf.Clamp(transform.position.y + moveY, -8.5f, 8.5f)
         );
-        transform.position += Vector3.right * _yoko_speed * Time.deltaTime;
+        transform.position += Vector3.right * _Yoko_speed * Time.deltaTime;
     }
 }
