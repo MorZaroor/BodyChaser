f7028066ebc7ecbfa459b927187da0e1481e9bbf
yyyoru <game22403018@gmail.com>
2024-11-26 11:32:54 +0900

    UIManagment

--- BodyChaser/Assets/rakougetu/StartGame.cs
+++ BodyChaser/Assets/rakougetu/StartGame.cs
@@ -1,18 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class StartGame : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void LoadNextScene()
     {
-        
+        //loud next scene
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
